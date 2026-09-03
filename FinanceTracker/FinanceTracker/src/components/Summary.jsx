dfa72a6f5fbe584405d976c3f68f8314dabe63b1
R8152 <raulteles05@gmail.com>
2026-09-03 15:40:11 -0300

    feat: adiciona estrutura do sumário

--- FinanceTracker/FinanceTracker/src/components/Summary.jsx
+++ FinanceTracker/FinanceTracker/src/components/Summary.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
-export default function Summary() {
+export default function Summary({income = 0, outcome = 0, total = 0}) {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        <h3>Receitas</h3>
+        <span></span>
+      </div>
+      <div>
+        <h3>Despesas</h3>
+        <span></span>
+      </div>
+      <div>
+        <h3>Saldo Atual</h3>
+        <span></span>
+      </div>
+    </>
   )
 }
