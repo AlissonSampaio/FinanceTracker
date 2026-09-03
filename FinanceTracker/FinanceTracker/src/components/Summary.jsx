06e839894ea58960b605290ba01dee232ec9654d
R8152 <raulteles05@gmail.com>
2026-09-03 15:47:30 -0300

    feat: adiciona formatação das receitas, despesas e saldo atual

--- FinanceTracker/FinanceTracker/src/components/Summary.jsx
+++ FinanceTracker/FinanceTracker/src/components/Summary.jsx
@@ -1,17 +1,19 @@
+import { formatCurrency } from "../utils/formatCurrency";
+
 export default function Summary({income = 0, outcome = 0, total = 0}) {
   return (
     <>
       <div>
         <h3>Receitas</h3>
-        <span></span>
+        <span>{formatCurrency(income)}</span>
       </div>
       <div>
         <h3>Despesas</h3>
-        <span></span>
+        <span>{formatCurrency(outcome)}</span>
       </div>
       <div>
         <h3>Saldo Atual</h3>
-        <span></span>
+        <span>{formatCurrency(total)}</span>
       </div>
     </>
   )
