f5f7dd438eae891f85ac8dcb20ff92d6d00edc3f
R8152 <raulteles05@gmail.com>
2026-09-11 10:28:11 -0300

    feat: adiciona função de buscar transações na aplicação

--- FinanceTracker/FinanceTracker/src/App.jsx
+++ FinanceTracker/FinanceTracker/src/App.jsx
@@ -5,9 +5,18 @@ import Transactions from "./pages/Transactions";
 import TransactionNew from "./pages/TransactionNew";
 import TransactionDetails from "./pages/TransactionDetails";
 import NotFound from "./pages/NotFound";
+import { useEffect, useState } from "react";
 
 function App() {
-
+  // Busca transações do LocalStorage; se não houver transações, retorna um array vazio
+  const [transactions, setTransactions] = useState(() => {
+    const saved = localStorage.getItem('@finance-tracker:transactions');
+    return saved ? JSON.parse(saved) : [];
+  })
+  // Sincroniza alterações com LocalStorage
+  useEffect(() => {
+    localStorage.setItem('@finance-tracker:transactions', JSON.stringify(transactions));
+  }, [transactions]);
   return (
     <>
       <BrowserRouter>
