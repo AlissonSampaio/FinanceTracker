e070885b6bc57e63336f1b63982838f523bc39e7
R8152 <raulteles05@gmail.com>
2026-09-21 19:47:10 -0300

    feat: cria seção de filtros no esqueleto da página de transações

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { CATEGORIES } from "../categories/categories";
 
 export default function Transactions() {
   // Filtros do tipo e da categoria
@@ -21,7 +22,25 @@ export default function Transactions() {
         <h1>Transações</h1>
         <Link to="/transactions/new">+ Nova Transação</Link>
       </header>
-      
+      <section>
+        <div>
+          <label htmlFor="type-filter">Filtrar por Tipo:</label>
+          <select id="type-filter" value={selectedType} onChange={(e) => setSelectedType(e.target.value)}>
+            <option value="Todas">Todas</option>
+            <option value="Receitas">Receitas</option>
+            <option value="Despesas">Despesas</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="category-filter">Filtrar por Categoria:</label>
+          <select id="category-filter" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+            <option value="Todas">Todas as Categorias</option>
+            {CATEGORIES.map((category) => (
+              <option key={category} value={category}>{category}</option>
+            ))}
+          </select>
+        </div>
+      </section>
     </>
   )
 }
