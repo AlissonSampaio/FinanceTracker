d1d6d0462c38694e686d24ed548934b73d674166
R8152 <raulteles05@gmail.com>
2026-09-21 19:33:26 -0300

    feat: cria função para filtrar a lista principal de transações sem mudar dados originais

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -7,6 +7,14 @@ export default function Transactions() {
   const [selectedCategory, setSelectedCategory] = useState('Todas');
 
   const list = Array.isArray(transactions) ? transactions : []; //Garante que list seja um array seguro para evitar erros
+
+  const filteredTransactions = list.filter((transaction) => { //filter para analisar cada transação da lista original em sequência
+    // função que confirma se o tipo de transação corresponde ao filtro selecionado ou se a opção Todas foi selecionada
+    const matchesType = selectedType === 'Todas' || transaction.type === selectedType;
+    // função que confirma se a categoria corresponde à opção selecionada ou se é 'Todas' a opção selecionada
+    const matchesCategory = selectedCategory === 'Todas' || transaction.category === selectedCategory;
+    return matchesType && matchesCategory;
+  });
   return (
     <>
       <header>
